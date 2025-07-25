e5c33234f1bf3e59a70486ef293077cf65454047
Yashbhatt7 <yabh678@gmail.com>
2025-07-25 18:11:32 +0530

    Add

--- 1160.cpp
+++ 1160.cpp
@@ -1,39 +1,68 @@
-#include<cstddef>
 #include<iostream>
 #include<string>
+#include<map>
 #include<vector>
 
 int countCharacters(std::vector<std::string>& words, std::string chars) {
-    std::vector<std::string> n_str;
+    std::map<char, int> charCount;
+    bool check = true;
     int sum = 0;
-    int count = 0;
+    // std::unordered_map<char, int> charCount;
 
-    for(int i = 0; i < words.size(); ++i) {
-        for(int j = 0; j < words[i].size(); ++j) {
-            if(chars.size() < words[i].size()) {
-                break;
-            }
-            size_t pos = chars.find(words[i][j]);
-
-            if(pos == std::string::npos) {
-                std::string target = "adsf";
-                words.erase(std::remove(words.begin(), words.end(), target), words.end());
-            }
-        }
+    for(const auto& c : chars) {
+        ++charCount[c];
     }
 
-    for(int i = 0; i < n_str.size(); ++i) {
-        sum += n_str[i].size();
+    for(const auto& word : words) {
+        std::map<char, int> wordCount;
+
+        for(const auto& ch : word) {
+            ++wordCount[ch];
+        }
+
+        check = true;
+        for(const auto& [ch, count] : wordCount) {
+            if(charCount[ch] < count) {
+                check = false;
+                break;
+            }
+        }
+
+        if(check) {
+            sum += word.size();
+        }
     }
 
     return sum;
 }
 
 int main() {
-    // std::vector<std::string> words = { "cat","bt","hat","tree" };
-    // std::string chars = "atach";
-    std::vector<std::string> words = { "hello","world","leetcode" };
-    std::string chars = "welldonehoneyr";
+    std::vector<std::string> words = { "cat","bt","hat","tree" };
+    std::string chars = "atach";
+    // std::vector<std::string> words = {
+    //     "dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin",
+    //     "ndqeyhhcquplmznwslewjzuyfgklssvkqxmqjpwhrshycmvrb",
+    //     "ulrrbpspyudncdlbkxkrqpivfftrggemkpyjl",
+    //     "boygirdlggnh",
+    //     "xmqohbyqwagkjzpyawsydmdaattthmuvjbzwpyopyafphx",
+    //     "nulvimegcsiwvhwuiyednoxpugfeimnnyeoczuzxgxbqjvegcxeqnjbwnbvowastqhojepisusvsidhqmszbrnynkyop",
+    //     "hiefuovybkpgzygprmndrkyspoiyapdwkxebgsmodhzpx",
+    //     "juldqdzeskpffaoqcyyxiqqowsalqumddcufhouhrskozhlmobiwzxnhdkidr",
+    //     "lnnvsdcrvzfmrvurucrzlfyigcycffpiuoo",
+    //     "oxgaskztzroxuntiwlfyufddl",
+    //     "tfspedteabxatkaypitjfkhkkigdwdkctqbczcugripkgcyfezpuklfqfcsccboarbfbjfrkxp",
+    //     "qnagrpfzlyrouolqquytwnwnsqnmuzphne",
+    //     "eeilfdaookieawrrbvtnqfzcricvhpiv",
+    //     "sisvsjzyrbdsjcwwygdnxcjhzhsxhpceqz",
+    //     "yhouqhjevqxtecomahbwoptzlkyvjexhzcbccusbjjdgcfzlkoqwiwue",
+    //     "hwxxighzvceaplsycajkhynkhzkwkouszwaiuzqcleyflqrxgjsvlegvupzqijbornbfwpefhxekgpuvgiyeudhncv",
+    //     "cpwcjwgbcquirnsazumgjjcltitmeyfaudbnbqhflvecjsupjmgwfbjo",
+    //     "teyygdmmyadppuopvqdodaczob",
+    //     "qaeowuwqsqffvibrtxnjnzvzuuonrkwpysyxvkijemmpdmtnqx",
+    // };
+    //
+    // // std::string chars = "avyteswqppomeojxoybotzriuvxolmllevluauwb";
+    // std::string chars = "usdruypficfbpfbivlrhutcgvyjenlxzeovdyjtgvvfdjzcmikjraspdfp";
 
     std::cout << countCharacters(words, chars);
 
