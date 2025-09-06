378606e8e760bf5ee912a30e516ad001baeccb37
Yashbhatt7 <yabh678@gmail.com>
2025-09-06 11:19:43 +0530

    add

--- 1371.cpp
+++ 1371.cpp
@@ -1,35 +1,32 @@
 #include<iostream>
-#include<string>
-#include<unordered_map>
+#include<vector>
+#include<bitset>
 
 int findTheLongestSubstring(std::string s) {
-    std::unordered_map<char, int> frMap;
-    std::string str = "";
+    std::vector<int> first(32, -2);
+    first[0] = -1;
+    int state = 00000;
     int max = 0;
 
-    int left = 0;
     for (int i = 0; i < s.size(); ++i) {
-        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ) {
-            // std::cout << s[i] << "\n";
-            ++frMap[s[i]];
-            if (frMap[s[i]] > 2) {
-                while (s[left] != s[i]) {
-                    // std::cout << s[left] << "\n";
-                    ++left;
-                }
-                ++left;
-                std::cout << s[left] << "\n";
-                --frMap[s[i]];
-            }
+        switch (s[i]) {
+            case 'a': state ^= (1 << 0); break;
+            case 'e': state ^= (1 << 1); break;
+            case 'i': state ^= (1 << 2); break;
+            case 'o': state ^= (1 << 3); break;
+            case 'u': state ^= (1 << 4); break;
         }
-        if (max < i - left) {
-            max = i - left;
+
+        if (first[state] == -2) {
+            first[state] = i;
+        }
+
+        if (max <= i - first[state]) {
+            max = i - first[state];
         }
     }
 
-    // std::cout << "left: " << left << "\n";
-
-    return max + 1;
+    return max;
 }
 
 int main() {
