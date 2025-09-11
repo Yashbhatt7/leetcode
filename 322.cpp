a6b569b8290981dcbc15a03476bd1bca6cee0ca3
Yashbhatt7 <yabh678@gmail.com>
2025-09-12 04:19:15 +0530

    add

--- 322.cpp
+++ 322.cpp
@@ -1,32 +1,29 @@
 #include<iostream>
 #include<vector>
 
+#define INT_MAX 2147483647
+
 int coinChange(std::vector<int>& coins, int amount) {
-    int result = amount;
-    int max = 0;
-    int temp = result;
-    int count = 0;
+    if (amount == 0) return 0;
 
-    while (true) {
-        for (int i = 0; i < coins.size(); ++i) {
-            if (result >= coins[i]) {
-                temp -= coins[i];
+    std::vector<int> dp(amount + 1, INT_MAX);
+
+    dp[0] = 0;
+
+    int a = amount;
+    for (int i = 1; i <= amount; ++i) {
+        for (int j = 0; j < coins.size(); ++j) {
+            if (coins[j] <= i && dp[i - coins[j]] != INT_MAX) {
+                dp[i] = std::min(dp[i - coins[j]] + 1, dp[i]);
             }
-
-            if (max < temp) {
-                max = temp;
-                ++count;
-            }
-        }
-
-        result -= max;
-        temp = result;
-        if (result == 0) {
-            return count;
         }
     }
 
-    return -1;
+    if (dp[amount] == INT_MAX) {
+        return -1;
+    }
+
+    return dp[amount];
 }
 
 int main() {
