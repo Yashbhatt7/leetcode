06f421d04b744630003909d8fff1c77a41a78703
Yashbhatt7 <yabh678@gmail.com>
2025-09-20 19:44:58 +0530

    add

--- 413.cpp
+++ 413.cpp
@@ -2,29 +2,23 @@
 #include<vector>
 
 int numberOfArithmeticSlices(std::vector<int>& nums) {
-    if (nums.size() < 3) {
-        return 0;
-    }
-
+    int dp = 0;
     int count = 0;
-    bool check = true;
-
-    for (int i = 0; i < nums.size() - 1; ++i) {
-        int a = std::abs(nums[i + 1] - nums[i]);
-        if (a != 1) {
-            check = false;
+    for (int i = 2; i < nums.size(); ++i) {
+        if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+            dp = dp + 1;
+            count += dp;
+        } else {
+            dp = 0;
         }
     }
-
-    if (check) {
-        return ((nums.size() - 1) * (nums.size() - 2)) / 2;
-    }
-
     return count;
 }
 
 int main() {
     std::vector<int> nums = { 1,2,3,4,5,6 };
+    // std::vector<int> nums = { 1,2,3,8,9,10 };
+    // std::vector<int> nums = { 1, 2, 3, 4 };
 
     std::cout << numberOfArithmeticSlices(nums);
 
--- 78.cpp
+++ 78.cpp
@@ -1,13 +1,34 @@
 #include<iostream>
 #include<vector>
-#include<unordered_set>
 
 std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+    std::vector<std::vector<int>> allSubsets;
+    allSubsets.push_back({});
 
+    for (int num : nums) {
+        int n = allSubsets.size();
+        for (int i = 0; i < n; ++i) {
+            std::vector<int> subset = allSubsets[i];
+            subset.push_back(num);
+            allSubsets.push_back(subset);
+        }
+    }
+
+    return allSubsets;
 }
 
 int main() {
+    std::vector<int> nums = { 1,2,3 };
 
+    std::vector<std::vector<int>> vec = subsets(nums);
+
+    for (const auto& i : vec) {
+        std::cout << "[ ";
+        for (const auto& j : i) {
+            std::cout << j << ", ";
+        }
+        std::cout << " ]";
+    }
 
     std::cin.get();
 }
