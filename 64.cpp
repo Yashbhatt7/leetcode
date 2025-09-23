b9cc78c7db2319ba44f94f56a062a47e88e77e0f
Yashbhatt7 <yabh678@gmail.com>
2025-09-23 13:20:35 +0530

    add

---
+++ 64.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+#define INT_MAX 993452342
+
+// My First 2D DP Problem
+int minPathSum(std::vector<std::vector<int>>& grid) {
+    int r = grid.size();
+    int c = grid[0].size();
+
+    std::vector<std::vector<int>> dp(r, std::vector<int>(c, INT_MAX));
+
+    dp[0][0] = grid[0][0];
+    for (int i = 1; i < grid[0].size(); ++i) {
+        dp[0][i] = dp[0][i - 1] + grid[0][i];
+    }
+
+    for (int i = 1; i < grid.size(); ++i) {
+        dp[i][0] = dp[i - 1][0] + grid[i][0];
+    }
+
+    for (int i = 1; i < grid.size(); ++i) {
+        for (int j = 1; j < grid[0].size(); ++j) {
+            dp[i][j] = std::min(dp[i][j - 1] + grid[i][j], dp[i - 1][j] + grid[i][j]);
+        }
+    }
+    return dp[r - 1][c - 1];
+}
+
+int main() {
+    std::vector<std::vector<int>> grid = {
+        { 1,3,1 },
+        { 1,5,1 },
+        { 4,2,1 },
+    };
+
+    std::cout << minPathSum(grid);
+
+    std::cin.get();
+}
+
