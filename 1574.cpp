1a11458c68222abb7ccaff4b6481e6187ff3b554
Yashbhatt7 <yabh678@gmail.com>
2025-09-25 23:01:57 +0530

    failed

--- 1574.cpp
+++ 1574.cpp
@@ -3,43 +3,49 @@
 #include<algorithm>
 
 int findLengthOfShortestSubarray(std::vector<int>& arr) {
-    int count = 0;
+    std::vector<int> prefix;
+    std::vector<int> suffix;
 
-    if(arr.size() == 1) {
-        return count;
-    }
+    prefix.push_back(arr[0]);
 
-    for(int i = 0; i < arr.size();) {
-        if(i + 1 >= arr.size()){
+    for (int i = 1; i < arr.size(); ++i) {
+        if (arr[i] >= prefix.back()) {
+            prefix.push_back(arr[i]);
+        } else {
             break;
         }
+    }
+    if (prefix.size() == arr.size()) return 0;
 
-        if(arr[i] > arr[i + 1]) {
-            /*std::cout << arr[i] << "\n";*/
-            arr.erase(arr.begin() + i);
-            ++count;
-
-            if (i > 0) {
-                --i;
-            }
-        }
-        else {
-            ++i;
+    suffix.push_back(arr[arr.size() - 1]);
+    for (int i = arr.size() - 2; i >= 0; --i) {
+        if (arr[i] <= suffix.back()) {
+            suffix.push_back(arr[i]);
+        } else {
+            break;
         }
     }
-    /*std::cout << arr[0] << "\n";*/
-    /*std::cout << arr[1] << "\n";*/
-    /*std::cout << "\n\n";*/
+    std::reverse(suffix.begin(), suffix.end());
 
-    if(!std::is_sorted(arr.begin(), arr.end())) {
-        return findLengthOfShortestSubarray(arr);
+    int j = 0;
+    for (int i = prefix.size() - 1; i >= 0; --i) {
+        if (suffix[j] == 0) {
+            ++j;
+            continue;
+        }
+        if (prefix.back() > suffix[j]) {
+            prefix.erase(prefix.begin() + i);
+        }
+        if (!prefix.empty() && prefix.back() < suffix[j]) {
+            break;
+        }
     }
 
-    return count;
+    return arr.size() - (prefix.size() + suffix.size() + j);
 }
 
 int main() {
-    std::vector<int> arr { 1, 2, 3, 10, 4, 2, 3, 5 };
+    std::vector<int> arr  { 1, 2, 3, 10, 4, 2, 3, 5 };
     std::vector<int> arr3 { 1, 2, 3, 10, 0, 7, 8, 9 };
     std::vector<int> arr1 { 1, 2, 3 };
     std::vector<int> arr2 { 5, 4, 3, 2, 1 };
