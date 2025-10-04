5bd5da534be1766f371d23bd9403f563be7758de
Yashbhatt7 <yabh678@gmail.com>
2025-10-04 21:04:27 +0530

    add

--- 648.cpp
+++ 648.cpp
@@ -1,16 +1,38 @@
 #include<iostream>
 #include<vector>
+#include<sstream>
+#include<algorithm>
 
 std::string replaceWords(std::vector<std::string>& dictionary, std::string sentence) {
+    std::sort(dictionary.begin(), dictionary.end(),
+        [](const std::string& a, const std::string& b) {
+        return a.size() < b.size(); // shortest first
+        });
+    std::vector<std::string> fetchWords;
+    std::istringstream iss(sentence);
+    std::string word;
+    while (iss >> word) {
+        fetchWords.push_back(word);
+    }
 
-
-    for (int i = 0; i < dictionary.size(); ++i) {
-        for (int j = 0; j < sentence.size(); ++i) {
-
+    for (int j = 0; j < fetchWords.size(); ++j) {
+        for (int i = 0; i < dictionary.size(); ++i) {
+            int len = dictionary[i].size();
+            if (fetchWords[j].compare(0, len, dictionary[i]) == 0) {
+                fetchWords[j] = dictionary[i];
+                break;
+            }
         }
     }
 
-    return "";
+    std::string finalStr = "";
+    finalStr.reserve(sentence.size());
+    for (const auto& i : fetchWords) {
+        finalStr += i;
+        finalStr += ' ';
+    }
+    finalStr.pop_back();
+    return finalStr;
 }
 
 int main() {
